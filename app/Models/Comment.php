8e6c4dc01b85fb1cda4fe272de7191fc861e2971
moabit <orng101@gmail.com>
2018-03-11 00:05:19 +0100

    added gitkeep file

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,9 +4,12 @@
 namespace Filehosting\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Filehosting\Traits\MaterializedPathTrait;
 
 class Comment extends Model
 {
+    use MaterializedPathTrait;
+
     public $timestamps = false;
 
 }
